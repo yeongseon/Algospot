87e59d0a8f119ce8e0c750d06fab41e2c7dbfdd0
yeongseon choe <yeongseon.choe@gmail.com>
2016-07-01 10:58:48 +0900

    Add HELLOWORD

---
+++ Java/HELLOWORD/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		while(cases-- > 0) {
+			String name = sc.next();
+			System.out.println("Hello, " + name + "!");
+		}
+	}
+
+}
