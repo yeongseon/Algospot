76701cf71034463a58dbf77949093d7f3fe26586
yeongseon <yeongseon.choe@gmail.com>
2016-07-01 13:37:10 +0900

    Add HELLOWORD

---
+++ C++/HELLOWORD/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int cases;
+
+  cin >> cases;
+
+  while(cases--) {
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+  }
+}
